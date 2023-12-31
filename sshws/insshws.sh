bfd3c5acb490ad10c256725b15f0a9852baf85a7
free net <74614880+arismaramar@users.noreply.github.com>
2023-12-31 13:51:48 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,13 +4,34 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-openssh bebejje
-wget -O /usr/local/bin/ws-dropbear -##
-wget -O /usr/local/bin/ws-stunnel 8-2728+383+
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/sshws/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/sshws/ws-stunnel
 
 #izin permision
 #chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
-chmod +x /usr/local/bin/ws-stunnel 
-egebenenebsy
+chmod +x /usr/local/bin/ws-stunnel
+#chmod +x /usr/local/bin/ws-ovpn
+
+
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/arismaramar/AutoScriptXray-ssh/main/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+#restart service
+#
+systemctl daemon-reload
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+#Enable & Start & Restart ws-openssh service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
 
